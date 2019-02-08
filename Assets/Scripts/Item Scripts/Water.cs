7f108133b2167483404bdbcc636cb407ea8a386f
Jared Penner <jaredlpenner@gmail.com>
2019-02-07 19:01:38 -0800

    Pushing scripts

--- Assets/Scripts/Item Scripts/Water.cs
+++ Assets/Scripts/Item Scripts/Water.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Water : Item
 {
@@ -10,6 +11,8 @@ public class Water : Item
     // Start is called before the first frame update
     void Start()
     {
+        mClassManager = GameObject.Find("ClassManager").GetComponent<ClassManager>();
+        mDialogueUI = GameObject.Find("DialogueUI").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -21,7 +24,13 @@ public class Water : Item
     public override void interact()
     {
         Debug.Log("Adding " + potency + " to trip amount");
-        mDialogueUI.text = mDialogueText;
+        if (mClassManager.IsTripping())
+        {
+            mDialogueUI.text = mTrippingDialogueText;
+        } else
+        {
+            mDialogueUI.text = mDialogueText;
+        }
         mClassManager.increaseTrip(potency);
     }
 }
